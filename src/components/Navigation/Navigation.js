c62f1a183eff216f3e911b9de7260dd7f60b6639
plakmaks <romkazyeb@gmail.com>
2020-11-18 19:36:19 -0800

    First commit

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,36 +1,20 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-import useStyles from 'isomorphic-style-loader/useStyles';
 import React from 'react';
-import cx from 'classnames';
-import s from './Navigation.css';
-import Link from '../Link';
 
-export default function Navigation() {
-  useStyles(s);
-  return (
-    <div className={s.root} role="navigation">
-      <Link className={s.link} to="/about">
-        About
-      </Link>
-      <Link className={s.link} to="/contact">
-        Contact
-      </Link>
-      <span className={s.spacer}> | </span>
-      <Link className={s.link} to="/login">
-        Log in
-      </Link>
-      <span className={s.spacer}>or</span>
-      <Link className={cx(s.link, s.highlight)} to="/register">
-        Sign up
-      </Link>
-    </div>
-  );
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+    if (isSignedIn) {
+      return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+        </nav>
+      );
+    } else {
+      return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+          <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+        </nav>
+      );
+    }
 }
+
+export default Navigation;
